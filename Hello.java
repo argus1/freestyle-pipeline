b0fb786e1eebef20311874767dec744a19507cf0
argus1 <argus.sun@gmail.com>
2023-12-01 23:42:24 -0800

    Added Exception Catching

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
-class Hello {
+public class Hello {
 	public static void main(String[] args) {
-		System.out.println("This is a java app created using Docker");
+		try{
+			System.out.println("This is a java app created using Docker");
+		} catch (Exception e) {
+			System.out.println("Errors can happen, Let's keep at it");
+		}
 	}
 }
 
